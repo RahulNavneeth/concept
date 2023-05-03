690304ed010cceea1f90017bef99c179634f0c61
RahulNavneeth <rahulmnavneeth@gmail.com>
2023-05-03 09:16:28 +0530

    isprod-server

--- server/utils/constants.ts
+++ server/utils/constants.ts
@@ -1,2 +1,2 @@
-const IS_PRODUCTION: boolean = false;
-export const CLIENT_URL = !IS_PRODUCTION ? 'http://localhost:5173' : "<CLIENT_PRODUCTION_URL>";
+const IS_PRODUCTION: boolean = true;
+export const CLIENT_URL = !IS_PRODUCTION ? 'http://localhost:5173' : "https://surface-mocha.vercel.app/";
